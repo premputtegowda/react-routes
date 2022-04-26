c125424e11a2e108d87b8d6d18b469ed05614caa
Prem Puttegowda <pkpgowda@gmail.com>
2022-04-26 14:07:19 -0700

    URL parameters

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,31 @@
+import TeamLogo from './TeamLogo'
+import useTeamNames from '../hooks/useTeamNames'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
-    return (<div className="container">
-        Home
+    const {loading,  response: teamNames} = useTeamNames()
+    
+    
+    if (loading) return null
+    return (
+      
+
+        <div className='container'>
+            <h1 className='large-header'>
+                Hash History Basketball League
+            </h1>
+            <h3 className='header text-center'>
+                 Select a team
+            </h3>
+
+            { teamNames.map((id)=>(
+                <Link key={id} to={`/${id}`}>
+                    <TeamLogo id={id} width='125px' />
+                </Link>
+
+            ))
+
+            }
+      
     </div>)
 }
